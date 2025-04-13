c058494811cd12f42d8322a5d24b00b78ffac756
Raissa <uamr.ieadpe1@gmail.com>
2025-04-13 09:17:52 -0300

    Utilizando cookies no Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -47,11 +47,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body
     )
+  
+   let  sessionId = request.cookies.sessionId
+
+     if (!sessionId){
+      sessionId = randomUUID()
+      
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge:  60 * 60 * 24 * 7, // 7 dias
+     })
+     }
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send({ message: 'Transação criada com sucesso!' })
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
+import cookie from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions' // Certifique-se de que o caminho está correto
 
 const app = fastify()
 
+app.register(cookie)
 // Registra as rotas de transações
 app.register(transactionsRoutes, { prefix: '/transactions' }) // Adicione o prefixo
 
